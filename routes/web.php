535bcee7820e8d88feaaee3976f604e9ad1f1c20
isaiassm <isaias1113@outlook.com>
2018-12-20 23:03:53 -0200

    [UPDATE] Arquivo homecontroller, homeblade e welcomeblade foram deletados para que ouvesse uma nova configuracao e estrutura para melhor organizacao do projeto

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,14 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+$this->group(['middleware' => ['auth'], 'namespace' => 'Admin'], function(){
+    Route::get('admin', 'AdminController@index')->name('admin.home');
 });
+
+Route::get('/','Site\SiteController@index');
+//a contra barra esta indicando aonde esta a controller
 
 
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
