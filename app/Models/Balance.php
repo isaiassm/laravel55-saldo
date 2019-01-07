f1eab90522e38bfd2af0a1fabe8214f06161a760
isaiassm <isaias1113@outlook.com>
2019-01-07 15:41:44 -0200

    [UPDATE] ultilizando beginTransaction para quando for adicionar um valor, ele fazer o rollback caso não adicione em alguma das tabelas

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -3,13 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use DB;
 class Balance extends Model
 {
     public $timestamps = false;
 
     public function deposit(float $value) : Array
     {
+        DB::beginTransaction();
+
         $totalBefore = $this->amount ? $this->amount : 0;
         $this->amount += number_format($value, 2, '.','');
         $deposit = $this->save();
@@ -23,16 +25,23 @@ class Balance extends Model
         ]);
 
         if($deposit && $historic){
+
+            DB::commit();
+
             return [
                 'success' => true,
                 'messagem' => 'sucesso ao recarregar'
             ];
 
+        } else{
+
+            DB::rollback();
+
             return [
                 'success' => false,
                 'message' => 'falha ao carregar'
             ];
-        }
 
+        }
     }
 }
