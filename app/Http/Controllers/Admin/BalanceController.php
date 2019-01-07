fdad91155400993dfad14c3d60a2f524b3833ba6
isaiassm <isaias1113@outlook.com>
2019-01-07 16:23:21 -0200

    [UPDATE] ultilizando a view para retornar o erro, ultilizando o required para verificar se é do tipo numerico

--- app/Http/Controllers/Admin/BalanceController.php
+++ app/Http/Controllers/Admin/BalanceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Balance;
+use App\Http\Requests\MoneyValidationFormRequest;
 
 class BalanceController extends Controller
 {
@@ -21,11 +22,11 @@ class BalanceController extends Controller
     {
         return view('admin.balance.deposit');
     }
-    public function depositStore(Request $request, Balance $balance)
+    public function depositStore(MoneyValidationFormRequest $request)
     {
     
        $balance = auth()->user()->balance()->firstOrCreate([]);
-       $balance->deposit($request->value);
+       dd($balance->deposit($request->value));
     }
 
 }
--- resources/views/admin/balance/deposit.blade.php
+++ resources/views/admin/balance/deposit.blade.php
@@ -18,6 +18,14 @@
        <h3>Fazer recarga </h3>
         </div>
         <div class="box-body">
+        @if ($errors->any())
+            <div class ="alert alert-warning">
+            @foreach ($errors->all() as $error)
+                <p>{{ $error }}</p>
+            @endforeach
+            </div>
+        @endif
+
         <form method="POST" action="{{ route('deposit.store') }}">
         {!! csrf_field() !!}
         <div class="form-group">
