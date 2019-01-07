b1e36183e95f8b50f5987d7641a742fc5f3eb5ed
isaiassm <isaias1113@outlook.com>
2019-01-07 15:20:55 -0200

    [UPDATE] Refatorando tela que estava quebrada pois estava passando um valor null, funcionalidade de regarrecar funcionando

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -12,11 +12,11 @@ class Balance extends Model
     {
         $totalBefore = $this->amount ? $this->amount : 0;
         $this->amount += number_format($value, 2, '.','');
-        $deposit= $this->save();
+        $deposit = $this->save();
 
          $historic = auth()->user()->historics()->create([
-            'type'          => 'I',
-             'amount'       => '$value',
+             'type'         => 'I',
+             'amount'       => $value,
              'total_before' => $totalBefore,
              'total_after'  => $this->amount,
              'date'         => date('Ymd'),
