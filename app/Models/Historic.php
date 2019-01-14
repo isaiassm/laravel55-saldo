3c88399b080cdf253049a9748fec249367814f5d
isaiassm <isaias1113@outlook.com>
2019-01-14 15:45:31 -0200

    [UPDATE] Filtrando pelo usuario logado, ultilizando o metodo global scopeUserAuth

--- app/Models/Historic.php
+++ app/Models/Historic.php
@@ -27,6 +27,11 @@ class Historic extends Model
         return $types [$type];
     }
 
+    public function scopeUserAuth($query)
+    {
+        return $query->where('user_id', auth()->user()->id);
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
@@ -55,6 +60,9 @@ class Historic extends Model
             if (isset($data['type']))
                 $query->where('type', $data['type']);    
         })//->toSql(); dd($historics);
+       // ->where('user_id', auth()->user()-id)
+        ->userAuth()
+        ->with(['userSender'])
         ->paginate($totalPage);
 
         return $historics;
