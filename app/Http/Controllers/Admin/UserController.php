8a51865cbb14c3b406dbc9756a5c0094b7145386
isaiassm <isaias1113@outlook.com>
2019-01-15 16:28:19 -0200

    [ADD] upload de imagem, ainda precisando de correçoes pois esta recebendo o dado como null

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -14,6 +14,8 @@ class UserController extends Controller
 
     public function profileUpdate(Request $request)
     {
+        $user = auth()->user();
+        
         $data = $request->all();
 
         if($data['password'] != null)
@@ -22,7 +24,30 @@ class UserController extends Controller
         else
             unset($data['password']);
 
-        $update = auth()->user()->update($data);
+        $data['image'] = $user->image;
+        if($request->hasFile('image') && $request->file('image')->isValid()){
+            if($user->image)
+                $name = $user->image;
+            else    
+                $name = $user->id.kebab_case($user->name);
+            
+            $extenstion = $request->image->extension();
+            $nameFile = "{$name}.{$extenstion}";    
+           
+            $data['image'] = $nameFile;
+            
+            $upload = $request->image->storeAs('users', $nameFile);
+
+                if (!$upload)
+                    return redirect()
+                                ->back()
+                                ->with('error', 'Sucesso ao fazer upload da imagem');
+                                
+        
+        }
+        dd($data['image']);
+
+        $update = $user->update($data);
 
         if($update)
             return redirect()
--- resources/views/Site/profile/profile.blade.php
+++ resources/views/Site/profile/profile.blade.php
@@ -16,7 +16,7 @@
  @endif
 
 
-<form action="{{ route('profile.update') }}" method="POST">
+<form action="{{ route('profile.update') }}" method="POST" enctype="multipart/form-data">
     {!! csrf_field() !!}
     <div class = "form-group">
         <label for="name">Nome:</label>
